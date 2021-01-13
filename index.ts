9122ea8632146c4b9a72086c7cfb611c889dcc44
DAIKI NIMURA <nimuradaiki0730@gmail.com>
2021-01-13 17:44:11 +0900

    Add core script

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
-import core from './core'
-export default core
+import core, { SitemapConfig } from './core'
+export const siteMapper = async (config: SitemapConfig) => {
+  if (!config) {
+    throw new Error('Config is mandatory')
+  }
+  const coreMapper = new core(config)
+  coreMapper.preLaunch()
+  await coreMapper.sitemapMapper(config.pagesDirectory || '')
+  coreMapper.finish()
+}
